e089e48373019d90699f12c239709f321977728f
Emerson Ramirez <ramirezemerson1991@gmail.com>
2026-05-28 19:19:09 -0400

    feat: implement CRUD operations for UsersController in NestJS

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete, Put, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 
 interface User {
   id: number;
@@ -37,10 +37,59 @@ export class UsersController {
 
   @Get(':id')
   findUser(@Param('id') id: string){
-    if(this.users.find((user) => user.id === Number(id)) === undefined){
+    const user = this.users.find((user) => user.id === Number(id));
+    if(!user || typeof user === 'undefined'){
+      throw new NotFoundException(`User with ${id} not found`);
+    }
+    return user;
+  }
+
+  @Post()
+  createUser(@Body() body: User){
+    this.users.push(body);
+    return {
+      'message': 'User created successfully',
+      'user': body
+    }
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string){
+    const user = this.users.filter((user) => user.id === Number(id));
+    if(!user || typeof user === 'undefined'){
+      throw new NotFoundException(`User with ${id} not found`);
+    }
+    return {
+      'message': 'User deleted successfully',
+      'user': user
+    }
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() body: User){
+    const user = this.users.find((user) => user.id === Number(id));
+    if(!user || typeof user === 'undefined'){
       return {'message': 'User not found'};
     }
-    return this.users.find((user) => user.id === Number(id));
+    if(body.name){
+      user.name = body.name;
+    }
+    if(body.email && !body.email.includes('@')){
+      throw new UnprocessableEntityException('Invalid email');
+    }
+    else{
+      user.email = body.email;
+    }
+    if(body.password && body.password.length >= 6){
+      user.password = body.password;
+    }
+    else{
+      throw new UnprocessableEntityException('Invalid password');
+    }
+    return {
+      'message': 'User updated successfully',
+      'user': user
+    }
   }
 
 }
